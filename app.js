5901148eef701c9be979e8b2f9e57ca973275e32
nfcwebtr-maker <nfc.web.tr@gmail.com>
2026-01-17 17:37:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,32 +10,32 @@ document.addEventListener("DOMContentLoaded", function() {
     {
       name: "Başlangıç Setleri ve Standlar", // İsim güncellendi
       products: [
-        { id: "599", name: "SOS ve Sosyal Medya Başlangıç Seti", price: 1600, image: "assets/599.png" },
+        { id: "599", name: "SOS ve Sosyal Medya Başlangıç Seti", price: 1600, image: "assets/505.png" },
         { id: "598", name: "Anahtarlık Teşhir Standı -Boş-", price: 200, image: "assets/598.png" }
       ]
     },
     {
       name: "SOS ve Acil Durum Anahtarlıkları",
       products: [
-        { id: "510", name: "NFC 0 Pozitif Kan Grubu", price: 40, image: "assets/510.png" },
-        { id: "521", name: "SOS Acil Yardım", price: 40, image: "assets/521.png" },
-        { id: "522", name: "Medikal Bilgi Anahtarlık", price: 40, image: "assets/522.png" },
-        { id: "511", name: "NFC 0 Negatif Kan Grubu", price: 40, image: "assets/511.png" },
-        { id: "512", name: "NFC A Pozitif Kan Grubu", price: 40, image: "assets/512.png" },
-        { id: "513", name: "NFC A Negatif Kan Grubu", price: 40, image: "assets/513.png" },
-        { id: "514", name: "NFC B Pozitif Kan Grubu", price: 40, image: "assets/514.png" },
-        { id: "515", name: "NFC B Negatif Kan Grubu", price: 40, image: "assets/515.png" },
-        { id: "516", name: "NFC AB Pozitif Kan Grubu", price: 40, image: "assets/516.png" },
-        { id: "517", name: "NFC AB Negatif Kan Grubu", price: 40, image: "assets/517.png" }
+        { id: "510", name: "NFC 0 Pozitif Kan Grubu", price: 40, image: "assets/505.png" },
+        { id: "521", name: "SOS Acil Yardım", price: 40, image: "assets/505.png" },
+        { id: "522", name: "Medikal Bilgi Anahtarlık", price: 40, image: "assets/505.png" },
+        { id: "511", name: "NFC 0 Negatif Kan Grubu", price: 40, image: "assets/505.png" },
+        { id: "512", name: "NFC A Pozitif Kan Grubu", price: 40, image: "assets/505.png" },
+        { id: "513", name: "NFC A Negatif Kan Grubu", price: 40, image: "assets/505.png" },
+        { id: "514", name: "NFC B Pozitif Kan Grubu", price: 40, image: "assets/505.png" },
+        { id: "515", name: "NFC B Negatif Kan Grubu", price: 40, image: "assets/505.png" },
+        { id: "516", name: "NFC AB Pozitif Kan Grubu", price: 40, image: "assets/505.png" },
+        { id: "517", name: "NFC AB Negatif Kan Grubu", price: 40, image: "assets/505.png" }
       ]
     },
     {
       name: "Sosyal Medya Takip Anahtarlıkları",
       products: [
-        { id: "501", name: "NFC Instagram Pembe", price: 40, image: "assets/501.png" },
-        { id: "502", name: "NFC Instagram Mavi", price: 40, image: "assets/502.png" },
-        { id: "503", name: "NFC TikTok Takip", price: 40, image: "assets/503.png" },
-        { id: "504", name: "NFC Google Yorum", price: 40, image: "assets/504.png" }
+        { id: "501", name: "NFC Instagram Pembe", price: 40, image: "assets/505.png" },
+        { id: "502", name: "NFC Instagram Mavi", price: 40, image: "assets/505.png" },
+        { id: "503", name: "NFC TikTok Takip", price: 40, image: "assets/505.png" },
+        { id: "504", name: "NFC Google Yorum", price: 40, image: "assets/505.png" }
       ]
     },
     {
@@ -47,7 +47,7 @@ document.addEventListener("DOMContentLoaded", function() {
     {
       name: "Veteriner ve PetShop Anahtarlıkları", // İsim güncellendi
       products: [
-        { id: "531", name: "Pet Takip NFC", price: 40, image: "assets/531.png" }
+        { id: "531", name: "Pet Takip NFC", price: 40, image: "assets/505.png" }
       ]
     }
   ];
@@ -160,3 +160,4 @@ document.getElementById('createOrderBtn').addEventListener('click', () => {
 
 // Sayfa yüklendiğinde başlat
 document.addEventListener('DOMContentLoaded', renderUI);
+
